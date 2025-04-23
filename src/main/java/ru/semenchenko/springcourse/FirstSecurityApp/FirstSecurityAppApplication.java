659774775e842b3bedd627c7f2c4f02f80c676cf
Артём Семенченко <artem_991456@mail.ru>
2025-04-23 12:15:41 +0300

    AuthenticationProvider changed to UserDetailsService

--- src/main/java/ru/semenchenko/springcourse/FirstSecurityApp/FirstSecurityAppApplication.java
+++ src/main/java/ru/semenchenko/springcourse/FirstSecurityApp/FirstSecurityAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSecurityAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstSecurityAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSecurityAppApplication.class, args);
+    }
 
 }
--- src/main/java/ru/semenchenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/semenchenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package ru.semenchenko.springcourse.FirstSecurityApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.semenchenko.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import ru.semenchenko.springcourse.FirstSecurityApp.services.PersonDetailsService;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * @author Artyom Semenchenko
@@ -13,17 +17,29 @@ import ru.semenchenko.springcourse.FirstSecurityApp.security.AuthProviderImpl;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authenticationProvider(authProvider);
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(withDefaults());
+
+        http.userDetailsService(personDetailsService);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
